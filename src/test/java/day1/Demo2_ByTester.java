36136601c2fdf28c723b863a7ea1ce823e90c5f6
Bhagya54 <testingtrainer54@gmail.com>
2025-12-30 22:58:20 +0530

    Added one more syso - By Tester

--- src/test/java/day1/Demo2_ByTester.java
+++ src/test/java/day1/Demo2_ByTester.java
@@ -3,5 +3,6 @@ package day1;
 public class Demo2_ByTester {
     public static void main(String[] args) {
         System.out.println("Demo 2 - By Tester");
+        System.out.println("Adding one more syso by Tester");
     }
 }
